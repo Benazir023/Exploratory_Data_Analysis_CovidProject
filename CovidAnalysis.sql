e3d00fde8b1a15ca14714db1a721e6d021c3ea4b
Benazir <123881327+Benazir023@users.noreply.github.com>
2023-03-08 17:29:05 +0300

    update comments

--- CovidAnalysis.sql
+++ CovidAnalysis.sql
@@ -1,3 +1,9 @@
+/*
+Data exploration - Covid19
+Showcasing skills: JOINS, CTE, TEMP TABLES, CREATING VIEWS, CONVERTING DATA TYPES
+*/
+
+
 --Confirm data was imported correctly
 
 SELECT *
@@ -9,20 +15,23 @@ SELECT *
 FROM CovidVaccinations
 ORDER BY location,date
 
---Select the data that will be used
+
+--Select the data to start with
 
 SELECT location, date, total_cases, new_cases, total_deaths, population
 FROM CovidDeaths
 WHERE continent IS NOT NULL
 ORDER BY location,date
 
---Looking at total_cases vs total_deaths
---Shows the probability of death if one contracts C-19 in Kenya, 
+
+--Total_cases vs total_deaths
+--Shows the probability of death if one contracts C-19 in Kenya, sub Kenya with any other location
 
 SELECT location, date, total_cases, total_deaths, (total_deaths/total_cases)*100 AS death_percentage
 FROM CovidDeaths
 WHERE location = 'Kenya'
 ORDER BY date
+
 
 --Looking at total cases vs population
 --Shows the percentage of population that contracted C-19
@@ -32,7 +41,8 @@ FROM CovidDeaths
 --WHERE location = 'Kenya'
 ORDER BY date
 
---Looking at locations that had the highest infection rates compared to population
+
+--Locations with highest infection rates compared to population
 
 SELECT location, MAX(total_cases) AS highest_infection_count, population, MAX((total_cases/population))*100 AS infection_rate
 FROM CovidDeaths
@@ -40,7 +50,8 @@ WHERE continent IS NOT NULL -- and location = 'Kenya'
 GROUP BY location, population
 ORDER BY infection_rate DESC
 
---Showing locations with highest death count per population
+
+--Locations with highest death count per population
 
 SELECT location, MAX(CAST(total_deaths AS int)) AS total_death_count
 FROM CovidDeaths
@@ -49,8 +60,9 @@ WHERE continent IS NOT NULL
 GROUP BY location, population
 ORDER BY total_death_count DESC
 
---LET'S BREAK THINGS DOWN BY CONTINENT
---Showing continents with the highest death count per population
+
+--BREAK DOWN BY CONTINENT
+-- Continents with the highest death count per population
 
 SELECT continent, MAX(CAST(total_deaths AS int)) AS total_death_count
 FROM CovidDeaths
@@ -58,6 +70,7 @@ WHERE continent IS NOT NULL
 --WHERE location = 'Kenya'
 GROUP BY continent
 ORDER BY total_death_count DESC
+
 
 --GLOBAL NUMBERS
 --Per day
@@ -68,15 +81,17 @@ WHERE continent IS NOT NULL
 GROUP BY date
 ORDER BY date
 
+
 --Aggregate global number as at Feb 2023
 
 SELECT SUM(CAST (new_cases as int)) AS total_cases, SUM(CAST (new_deaths as int)) AS total_deaths, SUM(CAST(new_deaths as int))/SUM(new_cases)*100 AS death_percentage
 FROM CovidDeaths
 WHERE continent IS NOT NULL
 
+
 --Use CovidDeaths & CovidVaccinations combined
 
---Looking at total population vs vaccinations
+--Total population vs vaccinations
 
 SELECT dea.continent, dea.location, dea.date, dea.population, vac.new_vaccinations,SUM(CONVERT(int,vac.new_vaccinations)) 
 OVER (PARTITION BY dea.location ORDER BY dea.location, dea.date) AS rolling_count_vaccinations
@@ -88,7 +103,8 @@ JOIN CovidVaccinations AS vac
 WHERE dea.continent IS NOT NULL --AND dea.location = 'Kenya'
 ORDER BY location, date
 
---USE CTE(common table expression)
+
+--USE CTE(common table expression) to calculate the rolling_count using PARTITION BY 
 
 WITH PopvsVac (continent, location, date, population, new_vaccinations, rolling_count_vaccinations)
 AS
@@ -104,7 +120,8 @@ WHERE dea.continent IS NOT NULL
 SELECT *,(rolling_count_vaccinations/population)*100 AS vaccinated_population_percent
 FROM PopvsVac
 
---TEMP Table
+
+--Using TEMP Table to calculate the rolling_count
 
 DROP TABLE IF EXISTS #PercentPopulationVaccinated
 CREATE TABLE #PercentPopulationVaccinated
@@ -144,5 +161,4 @@ WHERE dea.continent IS NOT NULL
 
 SELECT *
 FROM PercentPopulationVaccinated
-
 
